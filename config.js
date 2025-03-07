346a0051850a5494081dc5802e545beb4da8e270
Abdaziz <sazkz9292@gmail.com>
2025-03-07 12:33:11 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const encryptedKey = "U2FsdGVkX19vDo+Trh03N/jUL0y/pvyh2l5BdoILTjEDwR3HHb6EG/7rSI52p9xJNTHpw0E9Wtk7/REHppPf0g==";
-const password = "3654464%%779766677777%4948546446hsus881uwjssppnzsjowowwowbbszzvjswi28222leslswkwiwisz67w2io2o2wnsn";  // غيّر هذا إلى كلمة سر قوية
+const password = "123";  // غيّر هذا إلى كلمة سر قوية
 
 // دالة فك التشفير
 async function decryptKey(encryptedText) {
